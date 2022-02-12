3f38949c016adc99a20806419efe9bbba3b7e24e
Sakib12345 <sakibalislam12345@gmail.com>
2022-02-12 22:42:29 +0600

    main net added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+const projectId = '2cc7f3a11ab641b9bf78eca6897ff32f'
 
 module.exports = {
   defaultNetwork: 'hardhat',
@@ -7,7 +8,12 @@ module.exports = {
       chainId: 1337, //config standard
     },
     rinkeby:{
-      url:'https://rinkeby.infura.io/v3/2cc7f3a11ab641b9bf78eca6897ff32f'
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts:[]
+    },
+    mainnet:{
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: []
     }
   },
   solidity: "0.8.4",
