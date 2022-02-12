99c8e244d59957fd0335db40923d2f2fb90d7d20
Sakib12345 <sakibalislam12345@gmail.com>
2022-02-12 22:39:13 +0600

    rinkeby added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat: {
+      chainId: 1337, //config standard
+    },
+    rinkeby:{
+      url:'https://rinkeby.infura.io/v3/2cc7f3a11ab641b9bf78eca6897ff32f'
+    }
+  },
   solidity: "0.8.4",
 };
