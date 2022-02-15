97b6dc7cacc8001165c23f680e4f10eaf29aebf8
Sakib12345 <sakibalislam12345@gmail.com>
2022-02-16 03:31:11 +0600

    contractadress added and make counter for counter

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,3 +6,14 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    //counters allow us to keep track of token IDs
+    //address of marketplace for NFTS to iteract
+    address contractAddress;
+
+    //OBJ: give the MFT market the ability to transact with tokens or change ownership
+    // setApprovalForAll allows us to do that with contract address
+}
