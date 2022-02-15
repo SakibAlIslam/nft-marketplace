b70109bd6e41ce2e9cf5f8a4541388035d049aa1
Sakib12345 <sakibalislam12345@gmail.com>
2022-02-16 03:11:42 +0600

    import done

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,2 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+//we will bring in the openzeppelin ERC721 NFT functionality
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
