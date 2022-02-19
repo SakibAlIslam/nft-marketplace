c5a3734bce997d0fe9fb8e1d85bf181951f9ec13
Sakib12345 <sakibalislam12345@gmail.com>
2022-02-19 12:33:09 +0600

    fetch market tokens function done

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -144,5 +144,14 @@ contract KBMarket is ReentrancyGuard {
 
         //looping over the number of items created (if number has not been sold populate the array)
         MarketToken[] memory items = new MarketToken[](unsoldItemCount);
+        for(uint i = 0; i < itemCount; i++){
+            if(idToMarketToken[i + 1].owner == address(0)){
+                uint currentId = i + 1;
+                MarketToken storage currentItem = idToMarketToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 }
